825014e439d10a1736ebd509c0fe969a9112de38
Stanley Jony <1jony2jony@gmail.com>
2021-06-08 17:48:40 -0300

    04.Interpolação e Require

--- php-arrays-strings/banco.php
+++ php-arrays-strings/banco.php
@@ -1,29 +1,6 @@
 <?php
 
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
-function depositar(array $conta, float $valorADepositar): array {
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-
-    return $conta;
-}
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
@@ -41,10 +18,10 @@ $contasCorrentes = [
 ];
 
 
-$$contasCorrentes['123.256.789-12'] = depositar($contasCorrentes['123.256.789-12'],900);
+//$$contasCorrentes['123.256.789-12'] = depositar($contasCorrentes['123.256.789-12'],900);
 $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'],500);
 $contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'],200);
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+    exibeMensagem("$cpf $conta[titular] $conta[saldo]");
 }
