028eaf0fa346dbce8a1a2f4dc2a39b4263cd9cca
Stanley Jony <1jony2jony@gmail.com>
2021-06-16 23:56:46 -0300

    Adicionando Card para Img no cadastro de filiado

--- secretaria-conv/resources/views/admin/filiados/create.blade.php
+++ secretaria-conv/resources/views/admin/filiados/create.blade.php
@@ -24,68 +24,81 @@
 @section('conteudo')
     <form method="post">
         @csrf
-        <div class="border">
-            <div class="mt-3 mb-3 mr-3 ml-3">
-                    <div class="col-8">
-                        <div class="form-row">
-                            <div class="col-8">
-                                <div class="form-group">
-                                    <label for="nome">Nome</label>
-                                    <input type="text" class="form-control" name="nome" id="nome"> <!-- o meu name="nome" é quem será recuperado no request-->
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label for="cpf">CPF</label>
-                                    <input type="text" class="form-control" name="cpf" id="cpf">
-                                </div>
+        <div class="border rounded">
+
+            <div class="row m-2">
+                <!--Formulário-->
+                <div class="col-md-6">
+                    <div class="form-row">
+                        <div class="col-8">
+                            <div class="form-group">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" name="nome" id="nome"> <!-- o meu name="nome" é quem será recuperado no request-->
                             </div>
                         </div>
-                        <div class="form-row">
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label for="nascimento">Data de Nascimento</label>
-                                    <input type="text" class="form-control" name="nascimento" id="nascimento">
-                                </div>
-                            </div>
-                            <div class="col-8">
-                                <div class="form-group">
-                                    <label for="esposa">Esposa</label>
-                                    <input type="text" class="form-control" name="esposa" id="esposa">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col-3">
-                                <div class="form-group">
-                                    <label for="comead">Nº COMEAD</label>
-                                    <input type="text" class="form-control" name="comead" id="comead">
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="form-group">
-                                    <label for="cgadb">Nº CGADB</label>
-                                    <input type="text" class="form-control" name="cgadb" id="cgadb">
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="form-group">
-                                    <label for="funcao">Função</label>
-                                    <select class="form-control">
-                                        <option value="1">pastor</option>
-                                        <option value="2">evangelista</option>
-                                        <option value="3">presbítero</option>
-                                        <option value="4">diácono</option>
-                                    </select>
-                                </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="cpf">CPF</label>
+                                <input type="text" class="form-control" name="cpf" id="cpf">
                             </div>
                         </div>
                     </div>
-                    <!----------------IMAGEM----------------->
-                    <!----------------FALTA IMPLEMENTAR----------------->
+                    <div class="form-row">
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="nascimento">Nascimento</label>
+                                <input type="text" class="form-control" name="nascimento" id="nascimento">
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="form-group">
+                                <label for="esposa">Esposa</label>
+                                <input type="text" class="form-control" name="esposa" id="esposa">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="comead">Nº COMEAD</label>
+                                <input type="text" class="form-control" name="comead" id="comead">
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="cgadb">Nº CGADB</label>
+                                <input type="text" class="form-control" name="cgadb" id="cgadb">
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="form-group">
+                                <label for="funcao">Função</label>
+                                <select class="form-control">
+                                    <option value="1">pastor</option>
+                                    <option value="2">evangelista</option>
+                                    <option value="3">presbítero</option>
+                                    <option value="4">diácono</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!--Foto do Formulário-->
+                <div class="col-md-3">
+                    <div class="card">
+
+                        <img src="/img/imagem-3x4.jpg" class="" width="160px" height="215px">
+
+                        <div class="card-body">
+                          <h5 class="card-title">Stanley jony</h5>
+                          <p class="card-text">Software Developer</p>
+                        </div>
+                      </div>
+                </div>
             </div>
         </div>
-        <button class="btn btn-primary btn-block mt-3">Cadastrar</button>
+        <button class="btn btn-primary btn-block">Cadastrar</button>
     </form>
 
 @endsection
