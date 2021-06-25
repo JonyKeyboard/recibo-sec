4825a24df62a0dcf70d423c4961f44e1373e02da
Stanley Jony <1jony2jony@gmail.com>
2021-06-25 00:55:40 -0300

    03. Definindo os comportamentos

--- php-oo/src/conta.php
+++ php-oo/src/conta.php
@@ -5,4 +5,34 @@ class Conta
     public $cpfTitular;
     public $nomeTitular;
     public $saldo = 100;
+
+    public function sacar(float $valorASacar)
+{
+    if ($valorASacar > $this->saldo) {
+        echo "Saldo indisponível";
+        return;
+    } 
+    $this->saldo -= $valorASacar;
+    
+}
+public function depositar(float $valorADepositar): void
+{
+    if ($valorADepositar < 0) {
+        echo "Valor precisa ser positivo";
+        return;
+    }
+
+    $this->saldo += $valorADepositar;
+}
+
+public function transferir(float $valorATransferir, Conta $contaDestino): void
+{
+    if ($valorATransferir > $this->saldo) {
+        echo "Saldo indisponível";
+        return;
+    } 
+
+    $this->sacar($valorATransferir);
+    $contaDestino->depositar($valorATransferir);
+}
 }
