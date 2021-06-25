dba1f9b997f58bdfcacc120ea7166bb55cc4cbb3
Stanley Jony <1jony2jony@gmail.com>
2021-06-25 01:14:25 -0300

    04. Protegendo os nossos dados

--- php-oo/src/conta.php
+++ php-oo/src/conta.php
@@ -2,37 +2,62 @@
 
 class Conta
 {
-    public $cpfTitular;
-    public $nomeTitular;
-    public $saldo = 100;
+    private $cpfTitular;
+    private $nomeTitular;
+    private $saldo = 0;
 
-    public function sacar(float $valorASacar)
-{
-    if ($valorASacar > $this->saldo) {
-        echo "Saldo indisponível";
-        return;
-    } 
-    $this->saldo -= $valorASacar;
-    
-}
-public function depositar(float $valorADepositar): void
-{
-    if ($valorADepositar < 0) {
-        echo "Valor precisa ser positivo";
-        return;
+    public function saca(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
     }
 
-    $this->saldo += $valorADepositar;
-}
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
 
-public function transferir(float $valorATransferir, Conta $contaDestino): void
-{
-    if ($valorATransferir > $this->saldo) {
-        echo "Saldo indisponível";
-        return;
-    } 
+        $this->saldo += $valorADepositar;
+    }
 
-    $this->sacar($valorATransferir);
-    $contaDestino->depositar($valorATransferir);
-}
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function defineCpfTitular(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function defineNomeTitular(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+    public function recuperarNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
 }
