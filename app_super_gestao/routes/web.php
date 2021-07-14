2facbede98b29f545684f062f8c07c1aff979bc7
Stanley Jony <1jony2jony@gmail.com>
2021-07-14 07:33:35 -0300

    Testando o encaminhamento de parâmetros para a view

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -24,12 +24,13 @@ Route::get('/login', function(){ return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
 
-
     Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
 
 });
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 Route::fallback(function(){
     echo 'Rota inexistente. <a href="'.route('site.index').'">clique aqui</a>';
