ff188e62f81801b2297db093835620066dc71e6a
Stanley Jony <1jony2jony@gmail.com>
2022-05-10 14:25:16 -0300

    Protótipo de gerador pdf

--- pdf/gerarPdf.php
+++ pdf/gerarPdf.php
@@ -1,20 +1,17 @@
 <?php
 
-require_once 'dompdf/autoload.inc.php';
-//require __DIR__ . '/carta.html';
-
 use Dompdf\Dompdf;
-use Dompdf\Options;
 
-$options = new Options();
-$options->setChroot(__DIR__);
+// include autoloader
+require_once 'dompdf/autoload.inc.php';
 
-$dompdf = new Dompdf($options);
-//$dompdf->setHttpContext(fopen(__DIR__ . '../img/timbrado-topo.jpg', 'r'));
-$dompdf->loadHtmlFile(__DIR__.'\carta.html');
+
+
+ob_start();
+require  __DIR__ . '/carta.php';
+$dompdf = new Dompdf();
+$dompdf->loadHtml(ob_get_clean());
 $dompdf->render();
-
-header('Content-type: application/pdf');
-echo $dompdf->output();
+$dompdf->stream("file.pdf", ["Attachment" => false])
 
 ?>
