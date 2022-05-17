0585627be2c8e558c76303364c6e44d5177d896b
Stanley Jony <1jony2jony@gmail.com>
2022-05-17 16:47:56 -0300

    Excluido projeto crud-celkie 2.0

--- crud-php/js/custom.js
+++
@@ -1,9 +0,0 @@
-const tbody = document.querySelector(".listar-usuarios");
-
-const listarUsuarios = async (pagina) => {
-    const dados = await fetch("./list.php?pagina=" + pagina);
-    const resposta = await dados.text();
-    tbody.innerHTML = resposta;
-}
-
-listarUsuarios(1);
